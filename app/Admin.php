4e2d7809c64ca463f004299642407f031a45a2ee
konthanan <kumgumpon_k@silpakorn.edu>
2021-04-01 09:13:49 +0700

    add complete

---
+++ app/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Model;
+
+class Admin extends Model
+{
+    //
+    protected $guarded = ['id'];
+    protected $hidden = ['password','remember_token'];
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+}
